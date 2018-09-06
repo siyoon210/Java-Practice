cc3e7f2685e6fa2fb18d49475327180ea45d85a1
siyoon210 <jung_210@naver.com>
2018-09-06 19:38:35 +0900

    로또머신 클래스 만듬

--- src/fastcampus/lottoMachine/LottoMachine.java
+++ src/fastcampus/lottoMachine/LottoMachine.java
@@ -1,4 +1,27 @@
 package fastcampus.lottoMachine;
 
+import java.util.Collections;
+import java.util.List;
+
 public class LottoMachine {
+    private List<LottoBall> ballList;
+
+    public LottoMachine(List<LottoBall> ballList) {
+        this.ballList = ballList;
+    }
+
+    //공을 뽑는다.
+    public List<LottoBall> getBallList() {
+        return ballList;
+    }
+
+    public void setBallList(List<LottoBall> ballList) {
+        this.ballList = ballList;
+    }
+
+    //공을 섞는다.
+    public void shuffleBalls(){
+        Collections.shuffle(ballList);
+    }
+
 }

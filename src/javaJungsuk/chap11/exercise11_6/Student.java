4716cc5eddad8d2a5507692b840119556adc0c5e
siyoon210 <jung_210@naver.com>
2018-09-10 16:39:21 +0900

    자바의정석 TreeSet 예제 11-26
    연습문제 11_6시도하다가 못풀고 넘어옴

---
+++ src/javaJungsuk/chap11/exercise11_6/Student.java
@@ -0,0 +1,44 @@
+package javaJungsuk.chap11.exercise11_6;
+
+public class Student implements Comparable{
+    String name;
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+    Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+    int getTotal() {
+        return kor+eng+math;
+    }
+    float getAverage() {
+        return (int)((getTotal()/ 3f)*10+0.5)/10f;
+    }
+    public String toString() {
+        return name
+                +","+ban
+                +","+no
+                +","+kor
+                +","+eng
+                +","+math
+                +","+getTotal()
+                +","+getAverage()
+                ;
+    }
+    public int compareTo(Object o) {
+        if(o instanceof Student) {
+            Student tmp = (Student)o;
+            return name.compareTo(tmp.name);
+        } else {
+            return -1;
+        }
+    }
+
+}

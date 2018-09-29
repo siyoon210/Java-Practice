ba8e1e2c2157dfea48e37b3e74b5a308439a82a7
siyoon <jung_210@naver.com>
2018-09-29 23:56:33 +0900

    자바의정석 스택큐 실습

--- src/fastcampus/Reflection/ReflectionExam.java
+++ src/fastcampus/Reflection/ReflectionExam.java
@@ -1,4 +1,28 @@
 package fastcampus.Reflection;
 
-public class ReflectionExam {
+import java.lang.reflect.Method;
+
+class TestClass{
+    private String name;
+    private String hello = "hello";
+
+    public TestClass() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHello() {
+        return hello;
+    }
 }
+public class ReflectionExam {
+    public static void main(String[] args) throws Exception{
+        Class c = Class.forName("fastcampus.Reflection.TestClass");
+        Object o  = c.newInstance();
+        //형변환 어케 하는거자ㅣ?
+
+    }
+}
+

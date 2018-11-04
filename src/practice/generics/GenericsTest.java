cbcaa1c09ee17e68b4cad93eb9a244a8467379c3
siyoon <jung_210@naver.com>
2018-11-04 12:08:32 +0900

    stuydy: about generic wildecard

--- src/practice/generics/GenericsTest.java
+++ src/practice/generics/GenericsTest.java
@@ -17,6 +17,12 @@ public class GenericsTest {
 //        MyC다lass2<String> myClass2 = new MyClass2<String>(); //Number와 그의 자손이 아니면 사용 할 수 없
 
     }
+
+//    public void myMethod1(MyClass<T extends Number> myClass) { //T라는 키워드는 메소드 선언당시 아예 안됨 T는 제네릭 클래스를 선언할때 타입변수 제한을 쓸떄 사용한다.
+//    }
+
+    public void myMethod2(MyClass<? extends Number> myClass) {
+    }
 }
 
 class MyClass<T>{ //타입변수로 아무거나 다받음

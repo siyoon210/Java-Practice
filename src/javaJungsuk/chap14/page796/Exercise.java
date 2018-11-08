40ca35d6d4de934d960d21039fa69ed9240d1ec2
siyoon <jung_210@naver.com>
2018-11-08 20:38:03 +0900

    stury:rambda

---
+++ src/javaJungsuk/chap14/page796/Exercise.java
@@ -0,0 +1,39 @@
+package javaJungsuk.chap14.page796;
+
+public class Exercise {
+    int max(int a, int b) {
+        return a > b ? a : b;
+    }
+//    (a,b)-> a>b > a:b
+
+
+    void printVar(String name, int i) {
+        System.out.println(name + "=" + i);
+    }
+//    (name, i) -> System.out.println(name+"="+i)
+
+    int square(int x) {
+        return x * x;
+    }
+//    x-> x*x
+
+    int roll() {
+        return (int) (Math.random() * 6);
+    }
+//    ()->(int)(Math.random()*6)
+
+    int sumArr(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+//    (int[] arr)->{int sum=0;
+//        for (int i : arr) {
+//            sum+=i;
+//        }
+//        return sum;
+//    }
+
+}

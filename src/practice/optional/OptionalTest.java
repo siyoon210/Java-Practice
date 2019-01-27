3c0815aec3bc6999bc2c4919ee23b28ad66f74d2
siyoon <jung_210@naver.com>
2019-01-27 17:52:51 +0900

    자바8 옵셔날 클래스

--- src/practice/optional/OptionalTest.java
+++ src/practice/optional/OptionalTest.java
@@ -1,4 +1,36 @@
 package practice.optional;
 
-public class Optional {
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class OptionalTest {
+    public static void main(String[] args) {
+//        String str = null;
+//        System.out.println(str.length());
+
+//        String str = null;
+//        if(str == null){
+//            System.out.println(0);
+//        }
+//        else {
+//            System.out.println(str.length());
+//        }
+        String str = null;
+        Optional<String> strOptional = Optional.ofNullable(str);
+        System.out.println(strOptional.orElse("").length());
+
+        Supplier<String> stringSupplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "기본값";
+            }
+        };
+
+        Supplier<Exception> exceptionSupplier = new Supplier<Exception>() {
+            @Override
+            public Exception get() {
+                return new Exception();
+            }
+        };
+    }
 }

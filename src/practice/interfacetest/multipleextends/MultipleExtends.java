cfebc1dde5c199a801928594129f075dfe153254
siyoon <jung_210@naver.com>
2019-02-24 14:37:22 +0900

    인터페이스 다이아몬드 상속(?) 해보기

--- src/practice/interfacetest/multipleextends/MultipleExtends.java
+++ src/practice/interfacetest/multipleextends/MultipleExtends.java
@@ -1,4 +1,33 @@
 package practice.interfacetest.multipleextends;
 
+interface SuperInterface{
+    void superMethod();
+}
+
+interface SubInterface1 extends SuperInterface{
+    void subMethod();
+
+    @Override
+    void superMethod();
+}
+
+interface SubInterface2 extends SuperInterface{
+    int subMethod();
+
+    @Override
+    void superMethod();
+}
+
+//interface BottomInterface extends SubInterface1, SubInterface2 {
+//    @Override
+//    void subMethod();
+//
+//    @Override
+//    void superMethod();
+//}
 public class MultipleExtends {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+    }
 }

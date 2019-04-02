3096a014a572644faae052fd2924f8c5fbb3360d
siyoon210 <siyoon210@gmail.com>
2019-04-02 13:03:34 +0900

    템플릿 콜백 패턴 연습용 예제

--- src/practice/templatecallbackpattern/TemplateCallback.java
+++ src/practice/templatecallbackpattern/TemplateCallback.java
@@ -1,4 +1,33 @@
 package practice.templatecallbackpattern;
 
+class MyClass {
+    void myMethod(PrintB printB) {
+        a();
+
+        printB.b();
+
+        c();
+    }
+
+    void a() {
+        System.out.println("A");
+    }
+
+    void c() {
+        System.out.println("C");
+    }
+}
+
+interface PrintB {
+    void b();
+}
+
+
 public class TemplateCallback {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+
+        myClass.myMethod(()-> System.out.println("B1"));
+        myClass.myMethod(()-> System.out.println("B2"));
+    }
 }

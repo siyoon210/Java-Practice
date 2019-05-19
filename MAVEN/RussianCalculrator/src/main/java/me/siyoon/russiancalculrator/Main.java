aad416453b10156124dfd797e3663e4f8484076d
siyoon <siyoon210@gmail.com>
2019-05-19 14:30:00 +0900

    러시안 계산기 프로젝트 생성

---
+++ MAVEN/RussianCalculrator/src/main/java/me/siyoon/russiancalculrator/Main.java
@@ -0,0 +1,11 @@
+package me.siyoon.russiancalculrator;
+
+public class Main {
+    public void say() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

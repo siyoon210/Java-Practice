39a8c5fc51025c078af8bc3d2e4f3563d7c6ec71
siyoon <siyoon210@gmail.com>
2019-06-09 19:44:19 +0900

    Integer는 127이하는 캐시로 같은 참조 128이상은 다른 참조다

--- src/practice/wrapperclass/PrimitiveVsReference.java
+++ src/practice/wrapperclass/PrimitiveVsReference.java
@@ -1,11 +1,27 @@
-package practice;
+package practice.wrapperclass;
 
-public class primitivevsreference {
+public class PrimitiveVsReference {
     public static void main(String[] args) {
-        Integer i1 = new Integer(1);
-        Integer i2 = new Integer(1);
+        Integer i1 = Integer.valueOf(128);//127까진 캐시로 같은 참조 이지만 128부턴 다른 참조다.
+        Integer i2 = Integer.valueOf(128);
+
+        int i = Integer.valueOf(1);
+        Integer integer = i;
+
+        System.out.println("i = " + i);
+        System.out.println("integer = " + integer);
+
+        String s1 = "hello";
+        String s2 = "hello";
+
 
         if (i1 == i2) {
+            System.out.println("같은 참조");
+        } else {
+            System.out.println("다른 참조");
+        }
+
+        if (s1 == s2) {
             System.out.println("같은 참조");
         } else {
             System.out.println("다른 참조");

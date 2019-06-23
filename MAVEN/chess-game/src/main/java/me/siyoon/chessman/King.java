0be34d4396f4a39f570e35b5a9345b18d5274131
siyoon <siyoon210@gmail.com>
2019-06-23 22:45:25 +0900

    킹 4방향으로 되어 있는거 8방향으로 수정

--- MAVEN/chess-game/src/main/java/me/siyoon/chessman/King.java
+++ MAVEN/chess-game/src/main/java/me/siyoon/chessman/King.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 /**
  * King(킹)의 행마 방식
- * 킹은 상하좌우 방향으로, 1칸씩만 움직일 수 있다.
+ * 킹은 상하좌우와 대각선 4방향으로 총 8방향을 1칸씩만 움직일 수 있다.
  */
 public class King extends Chessman {
     private final Set<Direction> movableDirections = new HashSet<>();
@@ -23,6 +23,10 @@ public class King extends Chessman {
         movableDirections.add(Down.getInstance(1));
         movableDirections.add(Left.getInstance(1));
         movableDirections.add(Right.getInstance(1));
+        movableDirections.add(UpLeft.getInstance(1));
+        movableDirections.add(UpRight.getInstance(1));
+        movableDirections.add(DownLeft.getInstance(1));
+        movableDirections.add(DownRight.getInstance(1));
     }
 
     @Override

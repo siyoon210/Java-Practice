af4e100076a7c0cb772853171188b5cd41eae1ff
siyoon <siyoon210@gmail.com>
2019-06-26 09:19:35 +0900

    체스판 초기화 과정 메소드 Board로 이동

--- MAVEN/chess-game/src/main/java/me/siyoon/ChessGame.java
+++ MAVEN/chess-game/src/main/java/me/siyoon/ChessGame.java
@@ -13,47 +13,8 @@ public class ChessGame {
         whiteTurn = true;
     }
 
-    public void initBoard() {
-        Board.a8.setChessman(new Rook(Chessman.Color.BLACK));
-        Board.b8.setChessman(new Knight(Chessman.Color.BLACK));
-        Board.c8.setChessman(new Bishop(Chessman.Color.BLACK));
-        Board.d8.setChessman(new Queen(Chessman.Color.BLACK));
-        Board.e8.setChessman(new King(Chessman.Color.BLACK));
-        Board.f8.setChessman(new Bishop(Chessman.Color.BLACK));
-        Board.g8.setChessman(new Knight(Chessman.Color.BLACK));
-        Board.h8.setChessman(new Rook(Chessman.Color.BLACK));
-
-        Board.a7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.b7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.c7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.d7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.e7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.f7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.g7.setChessman(new Pawn(Chessman.Color.BLACK));
-        Board.h7.setChessman(new Pawn(Chessman.Color.BLACK));
-
-
-        Board.a2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.b2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.c2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.d2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.e2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.f2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.g2.setChessman(new Pawn(Chessman.Color.WHITE));
-        Board.h2.setChessman(new Pawn(Chessman.Color.WHITE));
-
-        Board.a1.setChessman(new Rook(Chessman.Color.WHITE));
-        Board.b1.setChessman(new Knight(Chessman.Color.WHITE));
-        Board.c1.setChessman(new Bishop(Chessman.Color.WHITE));
-        Board.d1.setChessman(new Queen(Chessman.Color.WHITE));
-        Board.e1.setChessman(new King(Chessman.Color.WHITE));
-        Board.f1.setChessman(new Bishop(Chessman.Color.WHITE));
-        Board.g1.setChessman(new Knight(Chessman.Color.WHITE));
-        Board.h1.setChessman(new Rook(Chessman.Color.WHITE));
-    }
-
     public void run() {
-        initBoard();
+        Board.initBoard();
 
         while (true) {
             consoleView.printBoard();

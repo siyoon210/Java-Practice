a021ee651af37462a23860d6a0a7652de6041b78
siyoon <siyoon210@gmail.com>
2019-07-11 09:28:56 +0900

    Matcher의 find()는 마치 이터레이터의 hasNext()처럼 사용 되는구나
    한번 찾은거면 다음것을 찾고 다음것을 찾고 다음것을 찾고...

--- src/practice/regularexpression/RegularExpression.java
+++ src/practice/regularexpression/RegularExpression.java
@@ -8,14 +8,15 @@ public class RegularExpression {
     public static void main(String[] args) {
         Pattern p = Pattern.compile("c[a-z]");
         Matcher m = p.matcher("abcdefghijklmnopcace");
-//        if (m.matches()) {
-//            System.out.println("matches 됨");
-//        } else if (m.find()) {
-//            System.out.println("find 됨");
-//        }
+        if (m.matches()) {
+            System.out.println("matches 됨");
+        } else if (m.find()) {
+            System.out.println("find 됨");
+        }
 
+        Matcher m2 = p.matcher("abcdefghijklmnopcace");
         int count = 0;
-        while (m.find()) {
+        while (m2.find()) {
             count++;
         }
 

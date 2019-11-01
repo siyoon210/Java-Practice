f338c5879c3d44ae5a6a5e33f12a505731862b81
siyoon <siyoon210@gmail.com>
2019-11-01 12:29:22 +0900

    null도 == 로 동일성이 비교 가능하다. NPE 안터짐
    null == null is True

---
+++ src/practice/nullcomparetest/NullCompareTest.java
@@ -0,0 +1,10 @@
+package practice.nullcomparetest;
+
+public class NullCompareTest {
+    public static void main(String[] args) {
+        Integer integer = 1;
+        Integer nullInteger = null;
+
+        System.out.println("integer == nullInteger = " + (nullInteger == integer));
+    }
+}

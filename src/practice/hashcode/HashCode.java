c880454d47aaaacbeb2ab164f58bec6388290cad
siyoon <siyoon210@gmail.com>
2020-03-26 09:26:50 +0900

    예전에 잘못 기록한 HashCode 테스트삭제
    이퀄스는 해시코드를 이용해서 비교하는 것이 아닌데, 해시코드를 이용해서 비교한다고 착각하고 있었군

--- src/practice/hashcode/HashCode.java
+++ src/practice/hashcode/HashCode.java
@@ -2,33 +2,6 @@ package practice.hashcode;
 
 public class HashCode {
     public static void main(String[] args) {
-        Integer i = new Integer(10);
-        Integer i2 = new Integer(-10);
-        System.out.println(i.hashCode());
-        System.out.println(i2.hashCode());
-        Long l = new Long(20);
-        System.out.println(l.hashCode());
-        Double d = new Double(10.5);
-        System.out.println(d.hashCode());
-        String s = new String("hi");
-        String s2 = new String("hi");
-        System.out.println(s.hashCode());
-        System.out.println(s2.hashCode());
-
-        if (i.hashCode() == i2.hashCode()) {
-            System.out.println("i와 i2의 해쉬코드 값은 같다");
-        }
-        if (i == i2) {
-            System.out.println("i와 i2는 완전히 같은 참조다");
-        } else {
-            System.out.println("다른 참조다");
-        }
-
-        /**결론
-         * 1) 정수형 참조변수 Integer,Long의 해쉬코드 값은 밸류와 같다.
-         *
-         * 2) 다른 참조라도 해쉬코드값은 같을 수 있다. equals는 해쉬코드로만 비교하는 것이 아니였다.
-         * */
 
     }
 }

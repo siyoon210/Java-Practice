5e38ac71ec9712adc91eab4a5c9f083af2229ff8
siyoon <siyoon210@gmail.com>
2020-03-26 09:42:37 +0900

    해시코드를 재정의 하지 않으면, hashCode는 완전히 논리적으로 같은 객체라도 다른 값을 막 뱉어낸다.

--- src/practice/hashcode/HashCode.java
+++ src/practice/hashcode/HashCode.java
@@ -1,7 +1,21 @@
 package practice.hashcode;
 
+class MyClass {
+    int intVale;
+    String strVale;
+    double doubleValue;
+
+    public MyClass(int intVale, String strVale, double doubleValue) {
+        this.intVale = intVale;
+        this.strVale = strVale;
+        this.doubleValue = doubleValue;
+    }
+}
 public class HashCode {
     public static void main(String[] args) {
-
+        final MyClass myClass1 = new MyClass(10, "siyoon", 10.0);
+        System.out.println("myClass1.hashCode() = " + myClass1.hashCode());
+        final MyClass myClass2 = new MyClass(10, "siyoon", 10.0);
+        System.out.println("myClass2.hashCode() = " + myClass2.hashCode());
     }
 }

3c11a4b3e966bc08cf954af83eb08c201a3d9728
siyoon <siyoon210@gmail.com>
2020-07-01 18:38:57 +0900

    샘플 테스트 클래스 추가

---
+++ MAVEN/junit4-to-junit5/src/test/java/me/siyoon/testing/Junit4To5_2.java
@@ -0,0 +1,55 @@
+package me.siyoon.testing;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class Junit4To5_2 {
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("beforeClass");
+    }
+
+    @BeforeEach
+    public void before() {
+        System.out.println("before");
+    }
+
+    @Test
+    public void shouldMigrateTestToSimpleTest() {
+        Assert.assertEquals("expected", "expected");
+    }
+
+    @Test
+    @Disabled
+    public void ignoreTestToDisableTest() {
+
+    }
+
+    @Test
+    public void shouldStillSupportHamcrestMatchers() {
+        assertThat(1, equalTo(1));
+    }
+
+    @Test
+    public void shouldStillSupportAssume() {
+        Assume.assumeTrue(javaVersion() > 0);
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("afterClass");
+    }
+
+    private int javaVersion() {
+        return 8;
+    }
+}

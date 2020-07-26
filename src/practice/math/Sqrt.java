90e18869f72975d2171c5ecbaa58d49006c1988f
siyoon <siyoon210@gmail.com>
2020-07-27 08:41:14 +0900

    Math.sqrt test

---
+++ src/practice/math/Sqrt.java
@@ -0,0 +1,13 @@
+package practice.math;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        System.out.println("Math.sqrt(1) = " + Math.sqrt(1));
+        System.out.println("Math.sqrt(2) = " + Math.sqrt(2));
+        System.out.println("Math.sqrt(3) = " + Math.sqrt(3));
+        System.out.println("Math.sqrt(4) = " + Math.sqrt(4));
+        System.out.println("Math.sqrt(16) = " + Math.sqrt(16));
+        System.out.println("Math.sqrt(17) = " + Math.sqrt(17));
+        System.out.println("Math.sqrt(25) = " + Math.sqrt(25));
+    }
+}

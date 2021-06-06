b216e3e34e55c3a10f62e5efc4fdc2de926c45c1
siyoon210 <siyoon210@gmail.com>
2021-06-06 21:49:34 +0900

    옵저버패턴 구현해보기

---
+++ src/practice/observerpattern/ObserverPattern.java
@@ -0,0 +1,71 @@
+package practice.observerpattern;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class ObserverPattern {
+    public static void main(String[] args) {
+        Subject subject = new MyStringSubject();
+        Observer observer1 = new MyStringObserver("1");
+        Observer observer2 = new MyStringObserver("2");
+
+        subject.registerObserver(observer1);
+        subject.registerObserver(observer2);
+
+        subject.notifyObservers("안녕~~");
+
+        subject.unregisterObserver(observer1);
+
+        subject.notifyObservers("잘가~~");
+    }
+}
+
+interface Observer<T> {
+    void observe(T event);
+}
+
+
+class MyStringObserver implements Observer<String> {
+    private final String label;
+
+    public MyStringObserver(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public void observe(String event) {
+        System.out.println("My String Obeserver " + label + " : " + event);
+    }
+}
+
+interface Subject<T> {
+    void registerObserver(Observer<T> observer);
+
+    void unregisterObserver(Observer<T> observer);
+
+    void notifyObservers(T event);
+}
+
+class MyStringSubject implements Subject<String> {
+    private final Set<Observer<String>> observers = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void registerObserver(Observer<String> observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void unregisterObserver(Observer<String> observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(String event) {
+        observers.stream().forEach(observer -> {
+            observer.observe(event);
+        });
+    }
+}
+
+
+
